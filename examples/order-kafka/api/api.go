f34435d0ae4b5d89e894b1a45cdc10c82aa84949
JailtonJunior94 <jailton.junior94@outlook.com>
2025-03-19 09:10:57 -0300

    adicionado melhorias em batch do corte para melhor processamento

--- examples/order-kafka/api/api.go
+++ examples/order-kafka/api/api.go
@@ -4,7 +4,6 @@ import (
 	"context"
 	"encoding/json"
 	"log"
-	"math/rand"
 	"net/http"
 	"os"
 	"os/signal"
@@ -52,36 +51,36 @@ func (s *apiServer) Run() {
 		log.Fatal(err)
 	}
 
-	lotes := []int{12, 13, 16, 17, 18, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36, 37, 38, 39, 40, 41, 42, 43, 44, 45}
-	status := []string{"Sucesso", "ComErro", "SemMovimento", "JaCortadoAnteriormente"}
+	// lotes := []int{12, 13, 16, 17, 18, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36, 37, 38, 39, 40, 41, 42, 43, 44, 45}
+	// status := []string{"Sucesso", "ComErro", "SemMovimento", "JaCortadoAnteriormente"}
 
-	for lote := range lotes {
-		messages := []*messaging.Message{}
+	// for lote := range lotes {
+	// 	messages := []*messaging.Message{}
 
-		for i := 1; i <= 1_000; i++ {
-			message := ParametrosConclusaoCorte{
-				LogCorte: lotes[lote],
-				Status:   status[rand.Intn(len(status))],
-			}
+	// 	for i := 1; i <= 1_000; i++ {
+	// 		message := ParametrosConclusaoCorte{
+	// 			LogCorte: lotes[lote],
+	// 			Status:   status[rand.Intn(len(status))],
+	// 		}
 
-			jsonMessage, err := json.Marshal(message)
-			if err != nil {
-				log.Printf("Error marshaling message: %v", err)
-				continue
-			}
+	// 		jsonMessage, err := json.Marshal(message)
+	// 		if err != nil {
+	// 			log.Printf("Error marshaling message: %v", err)
+	// 			continue
+	// 		}
 
-			msg := &messaging.Message{
-				Headers: []messaging.Header{},
-				Body:    jsonMessage,
-			}
+	// 		msg := &messaging.Message{
+	// 			Headers: []messaging.Header{},
+	// 			Body:    jsonMessage,
+	// 		}
 
-			messages = append(messages, msg)
-		}
+	// 		messages = append(messages, msg)
+	// 	}
 
-		if err := producer.PublishBatch(ctx, "conclusao_corte", "", map[string]string{}, messages); err != nil {
-			log.Fatal(err)
-		}
-	}
+	// 	if err := producer.PublishBatch(ctx, "conclusao_corte", "", map[string]string{}, messages); err != nil {
+	// 		log.Fatal(err)
+	// 	}
+	// }
 
 	routes := []httpserver.Route{
 		httpserver.NewRoute(http.MethodPost, "/message", func(w http.ResponseWriter, r *http.Request) error {
